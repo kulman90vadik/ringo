9186df937deee3d2691ce8f9efeed4b0183a0ea1
Vadim Kulman <vadim.kulman90@mail.ru>
2022-01-26 01:28:04 +0300

    add partners/project/gifts

--- app/js/main.js
+++ app/js/main.js
@@ -11,14 +11,41 @@ window.addEventListener("load", function(){
                 return '<span class="' + className + '">' + (index + 1) + "</span>";
             },
         },
-        
         navigation: {
             nextEl: '.home__btn--next',
             prevEl: '.home__btn--prev',
             
         },
-    
+    });
+
+
+    new Swiper('.project-swiper', {
+        wrapperClass: 'project-swiper__wrapper',
+        slideClass: 'project-swiper__slide',
+        // slidesPerView: 2,
+        slidesPerView: "auto",
+        loop: true,
+        spaceBetween: 40,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.project-swiper__btn--next',
+            prevEl: '.project-swiper__btn--prev',
+        },
+    });
+
+
+    new Swiper('.partners-swiper', {
+        wrapperClass: 'partners-swiper__wrapper',
+        slideClass: 'partners-swiper__slide',
+        slidesPerView: 4,
+        loop: true,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.partners-swiper__btn--next',
+            prevEl: '.partners-swiper__btn--prev',
+        },
     });
 
     // new WOW().init();
+
 });
